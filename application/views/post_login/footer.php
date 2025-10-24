b612bca14c2e0315398a49f015588f38fa1af5c6
meettan <mondal.tanmoy@synergicsoftek.com>
2025-10-24 15:15:54 +0530

    chnage

--- application/views/post_login/footer.php
+++ application/views/post_login/footer.php
@@ -33,5 +33,7 @@
   <script src="<?=base_url()?>assets/js/dashboard.js"></script>
   <script src="<?=base_url()?>assets/js/data-table.js"></script>
   <!-- End custom js for this page-->
+
+
 </body>
 </html>
